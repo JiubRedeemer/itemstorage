7163613adf15676243574876f3397b7d1861b9b7
jiubredeemer <jiubredeemer@gmail.com>
2026-04-02 14:38:33 +0400

    PRO-0: add damage type

--- src/main/java/com/jiubredeemer/itemstorage/domain/model/common/DamageTypeEnum.java
+++ src/main/java/com/jiubredeemer/itemstorage/domain/model/common/DamageTypeEnum.java
@@ -7,6 +7,19 @@ public enum DamageTypeEnum {
     CRUSHING("Дробящий"),
     STABBING("Колющий"),
     CHOPPING("Рубящий"),
+    PIERCING("Колющий"),
+    SLASHING("Рубящий"),
+    BLUDGEONING("Дробящий"),
+    ACID("Кислотный"),
+    COLD("Холодом"),
+    FIRE("Огненный"),
+    FORCE("Силовой"),
+    LIGHTNING("Молнией"),
+    NECROTIC("Некротический"),
+    POISON("Ядовитый"),
+    PSYCHIC("Психический"),
+    RADIANT("Сияющий"),
+    THUNDER("Громовой"),
     NO("");
     final String name;
 
